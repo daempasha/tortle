e8604dd4cad554962cff5e0151388f8b41fdaaed
daempasha <daempasha@gmail.com>
2025-02-27 19:29:49 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    # Implement REPL (https://en.wikipedia.org/wiki/Read%E2%80%93eval%E2%80%93print_loop)
+    while True:
+        # Wait for user input
+        sys.stdout.write("$ ")
+        command = input()
+        print(f"{command}: command not found")
 
-    # Wait for user input
-    command = input()
-
-    print(f"{command}: command not found")
+    
 
 if __name__ == "__main__":
     main()
