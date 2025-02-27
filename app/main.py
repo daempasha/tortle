dd33448a2be2f32f95ee99d0a14f7ad33f234209
daempasha <daempasha@gmail.com>
2025-02-27 19:31:28 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,11 @@ def main():
         # Wait for user input
         sys.stdout.write("$ ")
         command = input()
-        print(f"{command}: command not found")
 
+        if command == "exit 0":
+            exit(0)
+        print(f"{command}: command not found")
+        
     
 
 if __name__ == "__main__":
